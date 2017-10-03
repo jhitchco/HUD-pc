7c5db6e355650c7e4d7bbe078d98b5860c7b474b
Devon Bray <dev@esologic.com>
2017-10-03 17:33:49 -0400

    added rpi-gpio package

--- main.js
+++ main.js
@@ -11,6 +11,19 @@ const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const url = require('url');
 
+var gpio = require('rpi-gpio');
+ 
+gpio.setup(7, gpio.DIR_OUT, write);
+ 
+function write() {
+    gpio.write(7, true, function(err) {
+        if (err) throw err;
+        console.log('Written to pin');
+    });
+}
+
+write();
+
 var arduinoProcessReady = true;
 
 const processConfig = {
