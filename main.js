0a608d40334e29dabb31740ca40ba10c069abd80
Devon Bray <dev@esologic.com>
2017-10-03 16:39:40 -0400

    removed osx-specific code

--- main.js
+++ main.js
@@ -75,14 +75,6 @@ app.on('window-all-closed', function () {
 	}
 })
 
-app.on('activate', function () {
-	// On OS X it's common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
-	if (mainWindow === null) {
-		createWindow()
-	}
-})
-
 // Listen for async message from renderer process
 ipcMain.on('renderer-to-main', (event, arg) => {
 	
